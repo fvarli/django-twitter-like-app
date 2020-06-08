e8285a7362b3936ede5f5108c69f4afe63f1c787
fvarli <fvarlii@gmail.com>
2020-06-09 01:02:03 +0300

    updates: tweets

---
+++ tweets/views.py
@@ -0,0 +1,32 @@
+from django.http import HttpResponse, Http404, JsonResponse
+from django.shortcuts import render
+
+from .models import Tweet
+
+
+def home_view(request, *args, **kwargs):
+    # return HttpResponse("<h1>Hello World!</h1>")
+    return render(request, 'pages/home.html', context={}, status=200)
+
+
+def tweet_detail_view(request, tweet_id, *args, **kwargs):
+    """
+    REST API VIEW
+    Consume by JavaScript or Swift/Java/iOS/Android
+    return json data
+    """
+
+    data = {
+        "id": tweet_id,
+    }
+
+    status = 200
+
+    try:
+        obj = Tweet.objects.get(id=tweet_id)
+        data['content'] = obj.content
+    except:
+        data['message'] = "Not found"
+        status = 404
+
+    return JsonResponse(data, status=status)   # json.dumps content_type='application/json'
