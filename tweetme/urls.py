84625bd41a2b5b62bea47d46a69bf140a5c72994
fvarli <fvarlii@gmail.com>
2020-07-06 02:44:47 +0300

    updates: tweets

--- tweetme/urls.py
+++ tweetme/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, re_path  # url()
 
 from tweets.views import (home_view,
+                          tweet_action_view,
                           tweet_delete_view,
                           tweet_detail_view,
                           tweet_list_view,
@@ -29,5 +30,6 @@ urlpatterns = [
     path('tweets', tweet_list_view),
     path('create_tweet', tweet_create_view),
     path('tweets/<int:tweet_id>', tweet_detail_view),
+    path('api/tweets/action', tweet_action_view),
     path('api/tweets/<int:tweet_id>/delete', tweet_delete_view),
 ]
--- tweets/serializers.py
+++ tweets/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 from django.conf import settings
-from . models import Tweet
+from .models import Tweet
 
 MAX_TWEET_LENGTH = settings.MAX_TWEET_LENGTH
 TWEET_ACTION_OPTIONS = settings.TWEET_ACTION_OPTIONS
@@ -11,16 +11,21 @@ class TweetActionSeralizer(serializers.Serializer):
     action = serializers.CharField()
 
     def validate_action(self, value):
-        value = value.lower().strip()   # "Like" -> "like"
+        value = value.lower().strip()  # "Like" -> "like"
         if not value in TWEET_ACTION_OPTIONS:
             raise serializers.ValidationError("This is not valid action for tweets")
         return value
 
 
 class TweetSerializer(serializers.ModelSerializer):
+    likes = serializers.SerializerMethodField(read_only=True)
+
     class Meta:
         model = Tweet
-        fields = ['content']
+        fields = ['id', 'content', 'likes']
+
+    def get_likes(self, obj):
+        return obj.likes.count()
 
     def validate_content(self, value):
         if len(value) > MAX_TWEET_LENGTH:
